fd14c5c71af2d5223d6932b3def7b9850eabffd5
SalimSoormally <salim.s@intnet.mu>
2025-10-29 02:40:54 +0400

    Update update_ip.py

--- update_ip.py
+++ update_ip.py
@@ -3,7 +3,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 from datetime import datetime
 
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
 creds = Credentials.from_service_account_file("credentials.json", scopes=SCOPES)
 client = gspread.authorize(creds)
 sheet = client.open("SS_IP_Log").sheet1
